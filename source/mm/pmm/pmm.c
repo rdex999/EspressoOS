df51ba4d3a27d0d9fcb12b2998803c4ea5793f34
rdex999 <didabom2150@gmail.com>
2025-01-26 17:13:35 +0200

    Optimized pmm_bitmap_alloc_blocks and pmm_bitmap_free_blocks, added comments.

--- source/mm/pmm/pmm.c
+++ source/mm/pmm/pmm.c
@@ -27,7 +27,7 @@ void pmm_init(multiboot_tag_mmap_t*)
 	memset(PMM_BITMAP, 0, 1024);
 }
 
-/* In both of these functions, for some reason, i need to explicitly say the "1" is an unsigned 64 bit integer */
+/* In both of these functions, for some reason, i need to explicitly say the "1" is an unsigned 64 bit integer (llu) */
 void pmm_bitmap_alloc(size_t block)
 {
 	PMM_BITMAP[block / PMM_BITMAP_BITS_IN_ENTRY] |= 1llu << (block % PMM_BITMAP_BITS_IN_ENTRY);
@@ -43,12 +43,19 @@ bool pmm_bitmap_is_free(size_t block)
 	return (PMM_BITMAP[block / PMM_BITMAP_BITS_IN_ENTRY] & (1llu << (block % PMM_BITMAP_BITS_IN_ENTRY))) == 0;
 }
 
-/* TODO: Optimize the for loops in pmm_bitmap_alloc_blocks and pmm_bitmap_free_blocks */
-
 void pmm_bitmap_alloc_blocks(size_t start_block, size_t count)
 {
-	size_t entry_index = start_block / PMM_BITMAP_BITS_IN_ENTRY;
-	int bit_index = start_block % PMM_BITMAP_BITS_IN_ENTRY;
+	/* 
+	 * This function has 4 steps
+	 * 	- 1 => Check if <start_block> and <count> both start and end at an entry index, if so, memset the memory to 1 and return.
+	 *	- 2 => Mark the bits in the first entry, so <start_block> will be aligned to a bitmap entry. 
+	 * 		   (If count is greater than the amount of bits left in the entry, mark the bits left in the entry)
+	 *	- 3 => If there are no more blocks to allocate, return. Else, check if we can use memset to set full bitmap entries.
+	 *	- 4 => Mark the unaligned bits in the last entry, as they were not allocated in the memset.
+	 */
+
+	size_t entry_index = start_block / PMM_BITMAP_BITS_IN_ENTRY;	/* The entry index for <start_block> in the bitmap */
+	int bit_index = start_block % PMM_BITMAP_BITS_IN_ENTRY;			/* The bit offset in <entry_index> for the first bit to allocate */
 
 	/* If <start_block> and <count> both start and end at an index, we can just memset the memory to 1 */
 	if(bit_index == 0 && count % PMM_BITMAP_BITS_IN_ENTRY == 0)
@@ -58,9 +65,8 @@ void pmm_bitmap_alloc_blocks(size_t start_block, size_t count)
 	}
 
 	/* Finish the first entry, then if there are more check if using memset is possible */
-	size_t first_entry_blocks = MIN(PMM_BITMAP_BITS_IN_ENTRY - bit_index, count);
-	for(size_t i = 0; i < first_entry_blocks; i++)
-		pmm_bitmap_alloc(start_block + i);
+	size_t first_entry_blocks = MIN(PMM_BITMAP_BITS_IN_ENTRY - bit_index, count);	/* The amount of bits to set in the first entry */
+	PMM_BITMAP[entry_index] |= ((1llu << first_entry_blocks) - 1) << bit_index;		/* Set <first_entry_blocks> bits, then shift them to their index in the entry */
 
 	++entry_index;											/* Finished the first entry, so go to the next one */
 	count -= first_entry_blocks;							/* Decrease the amount of blocks left to allocate */
@@ -71,22 +77,30 @@ void pmm_bitmap_alloc_blocks(size_t start_block, size_t count)
 		return;
 
 	/* If we can use memset to set full entries (uint64_t), do it. */
-	size_t full_entries = count / PMM_BITMAP_BITS_IN_ENTRY;
+	size_t full_entries = count / PMM_BITMAP_BITS_IN_ENTRY;		/* The amount of full entries we can set using memset. */
 	if(full_entries > 0)
 	{
 		memset(&PMM_BITMAP[entry_index], 0xFF, full_entries * PMM_BITMAP_BYTES_IN_ENTRY);
-		// entry_index += full_entries;
+		entry_index += full_entries;
 		count -= full_entries * PMM_BITMAP_BITS_IN_ENTRY;
 		start_block += full_entries * PMM_BITMAP_BITS_IN_ENTRY;
 	}
 
 	/* Allocate the blocks that were not allocated in the memset */
-	for(size_t i = 0; i < count; i++)
-		pmm_bitmap_alloc(start_block + i);
+	PMM_BITMAP[entry_index] |= (1llu << count) - 1llu;			/* Set the remaining bits in the last entry */
 }
 
 void pmm_bitmap_free_blocks(size_t start_block, size_t count)
 {
+	/* 
+	 * This function has 4 steps (similar to pmm_bitmap_alloc_blocks)
+	 * 	- 1 => Check if <start_block> and <count> both start and end at an entry index, if so, memset the memory to 0 and return.
+	 *	- 2 => Clear the bits in the first entry, so <start_block> will be aligned to a bitmap entry. 
+	 * 		   (If count is greater than the amount of bits left in the entry, clear the bits left in the entry)
+	 *	- 3 => If there are no more blocks to free, return. Else, check if we can use memset to clear full bitmap entries.
+	 *	- 4 => Clear the unaligned bits in the last entry, as they were not cleared in the memset.
+	 */
+
 	size_t entry_index = start_block / PMM_BITMAP_BITS_IN_ENTRY;
 	int bit_index = start_block % PMM_BITMAP_BITS_IN_ENTRY;
 
@@ -98,9 +112,8 @@ void pmm_bitmap_free_blocks(size_t start_block, size_t count)
 	}
 
 	/* Finish the first entry, then if there are more check if using memset is possible */
-	size_t first_entry_blocks = MIN(PMM_BITMAP_BITS_IN_ENTRY - bit_index, count);
-	for(size_t i = 0; i < first_entry_blocks; i++)
-		pmm_bitmap_free(start_block + i);
+	size_t first_entry_blocks = MIN(PMM_BITMAP_BITS_IN_ENTRY - bit_index, count);	/* The amount of bits to clear in the first entry */
+	PMM_BITMAP[entry_index] &= ~(((1llu << first_entry_blocks) - 1) << bit_index);	/* Clear <first_entry_blocks> bits, then shift them to their index in the entry */
 
 	++entry_index;											/* Finished the first entry, so go to the next one */
 	count -= first_entry_blocks;							/* Decrease the amount of blocks left to free */
@@ -111,18 +124,17 @@ void pmm_bitmap_free_blocks(size_t start_block, size_t count)
 		return;
 
 	/* If we can use memset to set full entries (uint64_t), do it. */
-	size_t full_entries = count / PMM_BITMAP_BITS_IN_ENTRY;
+	size_t full_entries = count / PMM_BITMAP_BITS_IN_ENTRY;		/* The amount of full entries we can clear using memset. */
 	if(full_entries > 0)
 	{
 		memset(&PMM_BITMAP[entry_index], 0, full_entries * PMM_BITMAP_BYTES_IN_ENTRY);
-		// entry_index += full_entries;
+		entry_index += full_entries;
 		count -= full_entries * PMM_BITMAP_BITS_IN_ENTRY;
 		start_block += full_entries * PMM_BITMAP_BITS_IN_ENTRY;
 	}
 
 	/* Free the blocks that were not freed in the memset */
-	for(size_t i = 0; i < count; i++)
-		pmm_bitmap_free(start_block + i);
+	PMM_BITMAP[entry_index] &= ~((1llu << count) - 1llu);		/* Clear the remaining bits in the last entry */
 }
 
 size_t pmm_bitmap_addr_to_block(uint64_t address)
