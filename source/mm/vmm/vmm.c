4b439977c7de05574c6edb57e93fbe731e911db1
rdex999 <didabom2150@gmail.com>
2025-02-10 20:34:43 +0200

    Temporary initializing the first page tables manualy in the vmm_init function, poorly explained in a large comment.

--- source/mm/vmm/vmm.c
+++ source/mm/vmm/vmm.c
@@ -22,24 +22,58 @@ uint64_t* g_pml4;
 int vmm_init()
 {
 	/* Allocate the physical memory of the kernel, including the reverse mapping. */
-	pmm_alloc_address(0, (size_t)VMM_REVERSE_MAP_END / PMM_BLOCK_SIZE);
+	pmm_alloc_address(0, DIV_ROUND_UP((size_t)VMM_REVERSE_MAP_END, PMM_BLOCK_SIZE));
 
-	g_pml4 = (uint64_t*)pmm_alloc();
-	if(g_pml4 == (uint64_t*)-1)
+	phys_addr_t pml4_paddr = pmm_alloc();
+	if(pml4_paddr == (phys_addr_t)-1)
 		return ERR_OUT_OF_MEMORY;
+
+	/* 
+	 * This part, which initiates the first paging structures is temporary, as i still need to create some kind 
+	 * of data structure for managing free virtual addresses.
+	 * The reason im initializing these paging structures here, is because of a bug which my horrible design caused.
+	 * When using vmm_map_physical_page/s(), it finds a free virtual address, then uses the vmm_map_virtual_to_physical_page/s()
+	 * function for mapping the physical address. The problem is when the virtual address thats given to 
+	 * vmm_map_virtual_to_physical_page/s() doesnt have paging structures initialized, the function will try to create them,
+	 * and in the process of creating them it finds a free virtual address. The virtual address it will find, was probably also found
+	 * in the first call to vmm_map_physical_page/s() because it wasnt able yet to "mark" it as allocated.
+	 */
+	g_pml4 = (uint64_t*)pml4_paddr;
+	vmm_set_virtual_of(pml4_paddr, (virt_addr_t)g_pml4);
+
+	phys_addr_t pdp_paddr = pmm_alloc();
+	if(pdp_paddr == (phys_addr_t)-1)
+		return ERR_OUT_OF_MEMORY;
+
+	g_pml4[0] = VMM_CREATE_TABLE_ENTRY(VMM_PAGE_P | VMM_PAGE_RW, pdp_paddr);
+	vmm_set_virtual_of(pdp_paddr, (virt_addr_t)pdp_paddr);
+
+	phys_addr_t pd_paddr = pmm_alloc();
+	if(pd_paddr == (phys_addr_t)-1)
+		return ERR_OUT_OF_MEMORY;
+
+	((uint64_t*)pdp_paddr)[0] = VMM_CREATE_TABLE_ENTRY(VMM_PAGE_P | VMM_PAGE_RW, pd_paddr);
+	vmm_set_virtual_of(pd_paddr, (virt_addr_t)pd_paddr);
+
+	phys_addr_t pt_paddr = pmm_alloc();
+	if(pt_paddr == (phys_addr_t)-1)
+		return ERR_OUT_OF_MEMORY;
+
+	((uint64_t*)pd_paddr)[0] = VMM_CREATE_TABLE_ENTRY(VMM_PAGE_P | VMM_PAGE_RW, pt_paddr);
+	vmm_set_virtual_of(pt_paddr, (virt_addr_t)pt_paddr);
 
 	/* Identity map kernel memory, including the reverse memory map */
 	int status = vmm_map_virtual_to_physical_pages(
 		0, 
 		0, 
 		VMM_PAGE_P | VMM_PAGE_RW,
-		VMM_REVERSE_MAP_SIZE / VMM_PAGE_SIZE
+		(size_t)VMM_REVERSE_MAP_END / VMM_PAGE_SIZE
 	);
 
 	if(status != SUCCESS)
 		return status;
 	
-	write_cr3((uint64_t)g_pml4);
+	write_cr3((uint64_t)pml4_paddr);
 
 	return SUCCESS;
 }
@@ -95,7 +129,11 @@ virt_addr_t vmm_find_free_pages(size_t count)
 	if(count == 0)
 		return (virt_addr_t)-1;
 
-	for(virt_addr_t vaddr = 0; vaddr < (virt_addr_t)-1 - VMM_PAGE_SIZE; vaddr += VMM_PAGE_SIZE)
+	for(
+		virt_addr_t vaddr = ALIGN_UP((virt_addr_t)VMM_REVERSE_MAP_END, VMM_PAGE_SIZE) + PMM_BLOCK_SIZE*1; 
+		vaddr < (virt_addr_t)-1 - VMM_PAGE_SIZE; 
+		vaddr += VMM_PAGE_SIZE
+	)
 	{
 		uint64_t* pde = vmm_get_pde(vaddr);
 		if(pde != NULL && VMM_GET_ENTRY_LU(*pde) >= VMM_PAGE_TABLE_LENGTH)
@@ -232,8 +270,9 @@ int vmm_map_virtual_to_physical_page(virt_addr_t vaddr, phys_addr_t paddr, uint6
 	if(pmm_is_free(aligned_paddr))
 		pmm_alloc_address(aligned_paddr, 1);
 
-	uint64_t* pte = vmm_get_pte(vaddr);
-	*pte = VMM_CREATE_TABLE_ENTRY(flags, aligned_paddr);
+	uint64_t pte = VMM_CREATE_TABLE_ENTRY(flags, aligned_paddr);
+	vmm_set_pte(vaddr, pte);
+
 	*pde = VMM_INC_ENTRY_LU(*pde);
 
 	return SUCCESS;
@@ -361,11 +400,18 @@ int vmm_alloc_pde(virt_addr_t address, uint64_t flags)
 	if(pde == NULL)
 		return ERR_PAGE_NOT_MAPPED;
 
-	phys_addr_t paddr = pmm_alloc();
-	if(paddr == (phys_addr_t)-1)
+	phys_addr_t pt_paddr = pmm_alloc();
+	if(pt_paddr == (phys_addr_t)-1)
 		return ERR_OUT_OF_MEMORY;
 
-	*pde = VMM_CREATE_TABLE_ENTRY(flags, paddr);
+	virt_addr_t pt_vaddr = vmm_map_physical_page(pt_paddr, VMM_PAGE_P | VMM_PAGE_RW);
+	if(pt_vaddr == (virt_addr_t)-1)
+	{
+		pmm_free(pt_paddr);
+		return ERR_OUT_OF_MEMORY;
+	}
+
+	*pde = VMM_CREATE_TABLE_ENTRY(flags, pt_paddr);
 
 	uint64_t* pdpe = vmm_get_pdpe(address);
 	*pdpe = VMM_INC_ENTRY_LU(*pdpe);
@@ -408,8 +454,15 @@ int vmm_free_pde(virt_addr_t address)
 		}
 	}
 	
+	phys_addr_t pt_paddr = VMM_GET_ENTRY_TABLE(*pde);
+	virt_addr_t pt_vaddr = vmm_get_virtual_of(pt_paddr);
+
 	/* Free the physical block the entry points to, and mark the entry as not preset. Basicaly free it. */
-	pmm_free(VMM_GET_ENTRY_TABLE(*pde));
+	pmm_free(pt_paddr);
+
+	/* Free the virtual address that was mapped to the physical address of the page table. */
+	vmm_unmap_page(pt_vaddr);
+
 	*pde = 0llu;
 
 	/* 
@@ -453,11 +506,18 @@ int vmm_alloc_pdpe(virt_addr_t address, uint64_t flags)
 	if(pdpe == NULL)
 		return ERR_PAGE_NOT_MAPPED;
 
-	phys_addr_t paddr = pmm_alloc();
-	if(paddr == (phys_addr_t)-1)
+	phys_addr_t pd_paddr = pmm_alloc();
+	if(pd_paddr == (phys_addr_t)-1)
 		return ERR_OUT_OF_MEMORY;
 
-	*pdpe = VMM_CREATE_TABLE_ENTRY(flags, paddr);
+	virt_addr_t pd_vaddr = vmm_map_physical_page(pd_paddr, VMM_PAGE_P | VMM_PAGE_RW);
+	if(pd_vaddr == (virt_addr_t)-1)
+	{
+		pmm_free(pd_paddr);	
+		return ERR_OUT_OF_MEMORY;
+	}
+
+	*pdpe = VMM_CREATE_TABLE_ENTRY(flags, pd_paddr);
 
 	uint64_t* pml4e = vmm_get_pml4e(address);
 	*pml4e = VMM_INC_ENTRY_LU(*pml4e);
@@ -499,8 +559,15 @@ int vmm_free_pdpe(virt_addr_t address)
 		}
 	}
 
+	phys_addr_t pd_paddr = VMM_GET_ENTRY_TABLE(*pdpe);
+	virt_addr_t pd_vaddr = vmm_get_virtual_of(pd_paddr);
+
 	/* Free the physical block the entry points to, and mark the entry as not preset. Basicaly free it. */
-	pmm_free(VMM_GET_ENTRY_TABLE(*pdpe));
+	pmm_free(pd_paddr);
+
+	/* Free the virtual address that the physical address of the page directory was mapped to */
+	vmm_unmap_page(pd_vaddr);
+
 	*pdpe = 0llu;
 
 	/* 
@@ -530,11 +597,18 @@ int vmm_alloc_pml4e(virt_addr_t address, uint64_t flags)
 {
 	uint64_t* pml4e = vmm_get_pml4e(address);
 	
-	phys_addr_t paddr = pmm_alloc();
-	if(paddr == (phys_addr_t)-1)
+	phys_addr_t pdp_paddr = pmm_alloc();
+	if(pdp_paddr == (phys_addr_t)-1)
 		return ERR_OUT_OF_MEMORY;
-	
-	*pml4e = VMM_CREATE_TABLE_ENTRY(flags, paddr);
+
+	virt_addr_t pdp_vaddr = vmm_map_physical_page(pdp_paddr, VMM_PAGE_P | VMM_PAGE_RW);
+	if(pdp_vaddr == (virt_addr_t)-1)
+	{
+		pmm_free(pdp_paddr);
+		return ERR_OUT_OF_MEMORY;
+	}
+
+	*pml4e = VMM_CREATE_TABLE_ENTRY(flags, pdp_paddr);
 
 	return SUCCESS;
 }
@@ -542,6 +616,13 @@ int vmm_alloc_pml4e(virt_addr_t address, uint64_t flags)
 void vmm_free_pml4e(virt_addr_t address)
 {
 	uint64_t* pml4e = vmm_get_pml4e(address);
-	pmm_free(VMM_GET_ENTRY_TABLE(*pml4e));
-}
 
+	phys_addr_t pdp_paddr = VMM_GET_ENTRY_TABLE(*pml4e);
+	virt_addr_t pdp_vaddr = vmm_get_virtual_of(pdp_paddr);
+
+	/* Free the physical address of the pdp. */
+	pmm_free(pdp_paddr);
+
+	/* Free the virtual address that the physical address of the pdp was mapped to. */
+	vmm_unmap_page(pdp_vaddr);
+}
