b74288a52f06240d072e370639401d39617895b2
rdex999 <didabom2150@gmail.com>
2025-02-21 16:45:16 +0200

    Started working on ACPI, i just need it to know which PCI access mechanism is available.

--- source/include/acpi/tables.h
+++ source/include/acpi/tables.h
@@ -15,27 +15,5 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "kernel/kernel.h"
-#include <string.h>
-#include <stdlib.h>
-#include "mm/pmm/pmm.h"
-#include "mm/vmm/vmm.h"
-#include "acpi/acpi.h"
+#pragma once
 
-#define VIDEO ((uint32_t*)0xA0000)
-
-#ifdef __cplusplus
-	extern "C"
-#endif
-void kernel_main(multiboot_info_t* mbd)
-{
-	multiboot_tag_mmap_t* mmap = (multiboot_tag_mmap_t*)mbd->find_tag(MULTIBOOT_TAG_TYPE_MMAP);
-	if(mmap == NULL)	/* Always do null checks people, you dont want a damn headache. */
-		while(true) { asm volatile("cli"); asm volatile("hlt"); }
-
-	pmm_init(mmap);
-	vmm_init();
-	acpi_init(mbd);
-
-	while(true) { asm volatile("cli"); asm volatile("hlt"); }
-} 
